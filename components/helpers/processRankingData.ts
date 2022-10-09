07d6394f348c7e845ab269a40e91f7ff0e80041f
Alex <fotoflo@gmail.com>
2022-10-09 12:34:43 +0800

    update for push

--- components/helpers/processRankingData.ts
+++ components/helpers/processRankingData.ts
@@ -1,3 +1,15 @@
-export default function processRankingData(data: object): string {
+type RankingData = {
+  datasets: DataSet[];
+};
+
+type DataSet = {
+  label: string;
+  data: number[];
+  backgroundColor: string;
+  borderColor: string;
+  borderWidth: number;
+};
+
+export default function processRankingData(data: object): RankingData {
   return data.items[0];
 }
