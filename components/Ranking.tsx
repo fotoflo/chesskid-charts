41db98182ffb9c31d88ac9162b17f3fd66975549
Alex <fotoflo@gmail.com>
2022-10-09 09:08:48 +0800

    styled components

--- components/Ranking.tsx
+++ components/Ranking.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 type Props = {};
 
@@ -6,10 +7,18 @@ const Ranking = (props: Props) => {
   return (
     <div>
       <p>ranking data</p>
-      <p>{JSON.stringify(props.data)}</p>
+      <Data>{JSON.stringify(props.data)}</Data>
       <p>ranking data</p>
     </div>
   );
 };
+
+const Data = styled("p")`
+  max-width: 700px;
+  // center the content
+  margin: 0 auto;
+  padding: 1em;
+  word-wrap: break-word;
+`;
 
 export default Ranking;
