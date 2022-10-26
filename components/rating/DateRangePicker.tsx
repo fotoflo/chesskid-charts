82194a194d8530d0c5f9f92f1b578368c1c81905
Alex Miller <fotoflo@gmail.com>
2022-10-26 09:22:45 +0800

    tightening the date picker

--- components/rating/DateRangePicker.tsx
+++ components/rating/DateRangePicker.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 type Props = {};
 
@@ -12,27 +13,50 @@ const DateRangePickerComponent = ({
   maxDate,
   onChangeStartDate,
   onChangeEndDate,
-  className,
 }) => {
   return (
     <>
-      <DatePicker
-        wrapperClassName={className}
-        selected={startDate}
-        minDate={minDate}
-        maxDate={maxDate}
-        onChange={onChangeStartDate}
-      />
+      <DatePickerContainer>
+        <Row>
+          <Col md={12}>
+            <StyledDatePicker
+              selectsStart
+              selected={startDate}
+              startDate={startDate}
+              endDate={endDate}
+              minDate={minDate}
+              maxDate={maxDate}
+              onChange={onChangeStartDate}
+            />
 
-      <DatePicker
-        wrapperClassName={className}
-        minDate={minDate}
-        maxDate={maxDate}
-        selected={endDate}
-        onChange={onChangeEndDate}
-      />
+            <StyledDatePicker
+              selectsEnd
+              selected={endDate}
+              startDate={startDate}
+              endDate={endDate}
+              minDate={minDate}
+              maxDate={maxDate}
+              onChange={onChangeEndDate}
+            />
+          </Col>
+        </Row>
+      </DatePickerContainer>
     </>
   );
 };
+
+const DatePickerContainer = styled(Container).attrs({
+  className: "flex-row",
+})`
+  border: 4px solid purple;
+`;
+
+const StyledDatePicker = styled(DatePicker).attrs({
+  className: "p-1",
+})`
+  display: inline;
+  border: 2px solid red;
+  width: 105px;
+`;
 
 export default DateRangePickerComponent;
